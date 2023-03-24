e013649f9ad6ff28cdaad393b74582f3d3a3ace1
COMP102S1S2 <cihn.the.meskalin@gmail.com>
2023-03-24 14:12:02 +0300

    code file was changed

--- code_1.py
+++ code_1.py
@@ -1,5 +1,12 @@
+import time
 # First line of our first code
 print("Hello world!")
 
 # New line is added
 print("Hey! I'm changing the file!")
+
+for i in range(5,-1,-1):
+    print("-->", i)
+    time.sleep(1)
+
+print("Rocket was launched!")
